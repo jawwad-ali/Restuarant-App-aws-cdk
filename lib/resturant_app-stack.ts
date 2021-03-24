6ca47f606e287d951fe6b36e718517f4808ae16c
jawwad-ali <alijawwad001@gmail.com>
2021-03-24 21:27:42 +0500

    Initial commit

---
+++ lib/resturant_app-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class ResturantAppStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
